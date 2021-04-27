1c987159b122eb8d9f9530b9c17c0e7fc3c03159
sungdoolim <sungdoolim@naver.com>
2021-04-27 13:25:16 +0900

    first commit numbaseball

---
+++ app/src/main/java/com/example/numbaseball/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.numbaseball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+//        val ran=Random()
+//        var num=ran.nextInt(8999);
+//        num+=1000
+//        println("---------------------------------------------------------------"+num)
+//        var numstr=num.toString()
+//        var sorting:Queue<Int>;
+//        sorting=LinkedList();
+//
+//        for(i in 0..numstr.length-1){
+//            println(i)
+//            println(numstr[i])
+//            sorting.add(Integer.parseInt(numstr[i].toString()))
+//        }
+//        println(sorting)
+
+
+
+    }
+}
